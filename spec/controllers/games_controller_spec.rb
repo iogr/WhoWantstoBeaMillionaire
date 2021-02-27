ae9ddcf546ce5a7bde34f362dd3d865b03a7726d
iogr <antgorbunov@gmail.com>
2021-02-27 17:43:55 +0300

    user tries to create second game test in games_controller spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -52,6 +52,22 @@ RSpec.describe GamesController, type: :controller do
       expect(flash[:notice]).to be
     end
 
+    # юзер пытается создать игру, не закончив старую
+    it 'user creates second game' do
+      # убедились что есть игра в работе
+      expect(game_w_questions.finished?).to be_falsey
+
+      # отправляем запрос на создание, убеждаемся что новых Game не создалось
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game)
+      expect(game).to be_nil
+
+      # и редирект на страницу старой игры
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
+
     # юзер видит свою игру
     it '#show game' do
       get :show, id: game_w_questions.id
